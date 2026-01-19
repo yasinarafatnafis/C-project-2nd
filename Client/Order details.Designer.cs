749721d670ab9d96f3b7cb4994768be5cf1c044a
SK ASHRAFUL ALI APU <23-54917-3@student.aiub.edu>
2026-01-19 22:32:23 +0600

    done the viewing order details successfully

--- Client/Order details.Designer.cs
+++ Client/Order details.Designer.cs
@@ -40,6 +40,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(776, 381);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
