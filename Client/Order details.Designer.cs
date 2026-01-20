94c554ab0bc6abb172b47c07e460213042992b57
SK ASHRAFUL ALI APU <23-54917-3@student.aiub.edu>
2026-01-21 00:58:00 +0600

    design changes

--- Client/Order details.Designer.cs
+++ Client/Order details.Designer.cs
@@ -30,38 +30,51 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 42);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(776, 381);
+            this.dataGridView1.Size = new System.Drawing.Size(776, 351);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(694, 400);
+            this.button1.Location = new System.Drawing.Point(694, 409);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 38);
             this.button1.TabIndex = 1;
             this.button1.Text = "Back";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Your Orders";
+            // 
             // Order_details
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(814, 459);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Order_details";
             this.Text = "Order_details";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,5 +82,6 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- DataAccess.cs
+++ DataAccess.cs
@@ -1,13 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
+
+using System.Data.SqlClient;
+
+using System.Configuration;
 
 namespace C__project
+
 {
+
     internal class DataAccess
+
     {
 
+        private SqlConnection con;
+
+        public DataAccess()
+
+        {
+
+            con = new SqlConnection(
+
+                ConfigurationManager.ConnectionStrings["OfficeDB"].ConnectionString
+
+            );
+
+        }
+
+        public DataTable ExecuteQueryTable(string sql)
+
+        {
+
+            using (SqlCommand cmd = new SqlCommand(sql, con))
+
+            {
+
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+
+                {
+
+                    DataTable dt = new DataTable();
+
+                    da.Fill(dt);
+
+                    return dt;
+
+                }
+
+            }
+
+        }
+
+        public int ExecuteDMLQuery(string sql)
+
+        {
+
+            using (SqlCommand cmd = new SqlCommand(sql, con))
+
+            {
+
+                con.Open();
+
+                int row = cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                return row;
+
+            }
+
+        }
+
     }
+
 }
